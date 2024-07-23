4107b7c415e2ed20e7fb6dd072f439231fe90e60
SolCaruso <caruso@ocelot.ltd>
2024-07-23 13:53:59 -0400

    adds popup

--- card.js
+++ card.js
@@ -1,10 +1,22 @@
+
 export function cardItem(drink) {
-    let cardLayout = `
-        <div class="transform transition-transform duration-300 hover:scale-101 hover:-translate-y-1 cursor-pointer md:my-6">
-            <img class="rounded-2xl w-full" src=${drink.strDrinkThumb} alt=${drink.strDrink}>
-            <h2 class="font-bold text-xl flex-wrap text-center sm:text-left px-2 py-4">${drink.strDrink}</h2>
-        </div>
-      `;
-    return cardLayout;
-  }
+  let cardLayout = `
+      <div class="transform transition-transform duration-300 hover:scale-101 hover:-translate-y-1 cursor-pointer md:my-6 card">
+          <img class="rounded-2xl w-full" src=${drink.strDrinkThumb} alt=${drink.strDrink}>
+          <h2 class="font-bold text-xl flex-wrap text-center sm:text-left px-2 py-4">${drink.strDrink}</h2>
+          <dialog id="cardDialog" class='rounded-md py-12 px-16'>
+              <p class='text-center p-10 text-3xl'>Insert message here</p>
+              <p class='text-center p-10 text-3xl'>Insert message here</p>
+              <ul>
+              </ul>
+              <div class='w-full flex justify-center text-center'>
+                  <button class="bg-gray-100 px-2 h-9 rounded hover:bg-gray-200 font-bold text-gray-600 w-1/2 m-1 btnClose">Close</button>
+                  <button class='bg-gray-100 px-2 h-9 rounded hover:bg-gray-200 font-bold text-gray-600 w-1/2 m-1'>Add to favourites</button>
+              </div>
+          </dialog>
+      </div>
+  `;
+  return cardLayout;
+}
+
   
--- main.js
+++ main.js
@@ -1,87 +1,99 @@
 import { cardItem } from "./card.js";
 
 function init() {
-    console.log("Fetching Mojito Cocktail API")
+    console.log("Fetching Mojito Cocktail API");
     const cards = document.getElementById("card");
     let randomUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=mojito`;
-    fetch (randomUrl)
+    fetch(randomUrl)
         .then((res) => {
             if (!res.ok) {
-                throw new Error("Something ain't right dog");
+                throw new Error("Error with initial API fetch");
             }
-
             return res.json();
         })
         .then((data) => {
             let df = new DocumentFragment();
             console.log(data);
             data.drinks.forEach(drink => {
-              let card = document.createElement("div");
-              card.classList.add("card");
-              let cardInfo = cardItem(drink);
-              card.innerHTML = cardInfo;
-              df.append(card);
+                let card = document.createElement("div");
+                card.classList.add("card");
+                let cardInfo = cardItem(drink);
+                card.innerHTML = cardInfo;
+                df.append(card);
             });
             cards.innerHTML = "";
             cards.appendChild(df);
         })
-
         .catch((err) => {
             console.log(err);
         });
+
+    // Dialog Popover
+    document.getElementById('card').addEventListener('click', openDialog);
 }
 
-(()=> {
+function openDialog(ev) {
+    if (ev.target.closest('.card')) {
+        const dialog = ev.target.closest('.card').querySelector('#cardDialog');
+        dialog.showModal();
+        let btn = dialog.querySelector('.btnClose');
+        btn.addEventListener('click', closeDialog);
+    }
+}
+
+function closeDialog(ev) {
+    const dialog = ev.target.closest('dialog');
+    dialog.close();
+    ev.stopPropagation();
+}
+
+(() => {
     document.getElementById("findButton").addEventListener("click", searchHandle);
 })();
 
 let inputError = document.getElementById("inputError");
 
-function searchHandle(ev) { 
+function searchHandle(ev) {
     ev.preventDefault();
     console.log("nameFind function triggered");
-    inputError.textContent = `` ;
+    inputError.textContent = ``;
     let userInput = document.getElementById("cocktailInput").value.trim();
-    document.getElementById("cocktailInput").value="";
+    document.getElementById("cocktailInput").value = "";
     if (userInput) {
         findCocktail(userInput);
-    }
-    else {
+    } else {
         inputError.textContent = 'Please enter a real cocktail name.';
     }
 }
 
 function findCocktail(userInput) {
-
-    console.log("Fetching from API")
+    console.log("Fetching from API");
     let cards = document.getElementById("card");
     cards.innerHTML = "";
     let url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userInput}`;
-    fetch (url)
+    fetch(url)
         .then((res) => {
             if (!res.ok) {
-                throw new Error("Something ain't right dog");
+                throw new Error("Error with input cocktail API fetch");
             }
-
             return res.json();
         })
         .then((data) => {
             let df = new DocumentFragment();
             console.log(data);
             data.drinks.forEach(drink => {
-              let card = document.createElement("div");
-              let cardInfo = cardItem(drink);
-              card.innerHTML = cardInfo;
-              df.append(card);
+                let card = document.createElement("div");
+                card.classList.add("card");
+                let cardInfo = cardItem(drink);
+                card.innerHTML = cardInfo;
+                df.append(card);
             });
             cards.innerHTML = "";
             cards.appendChild(df);
         })
-
         .catch((err) => {
             console.log(err);
         });
-
 }
 
 window.addEventListener("DOMContentLoaded", init);
